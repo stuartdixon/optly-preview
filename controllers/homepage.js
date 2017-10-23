82dfb39527834288a3b17a69e4c30708135aa524
Stuart <stuart.dixon@gmail.com>
2017-10-23 12:55:43 -0700

    removed token

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,10 +1,36 @@
 webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filter', function($scope, $modal, $http, $filter){
 
 	$scope.optly_api_base = "https://api.optimizely.com/v2";
-	$http.defaults.headers.common['Authorization'] = "Bearer 2:PrJ4b7BbPa8GuTn9gymPUB-zHjL1XIHT_P18OYYufslteqyc0lmk"
 
+	if (readCookie('optlyCred')) {
+		$http.defaults.headers.common['Authorization'] = readCookie('optlyCred')
+	} else {
+		$http.defaults.headers.common['Authorization'] = prompt("enter API token")
+		createCookie ('optlyCred', $http.defaults.headers.common['Authorization'], 2)
+	}
+	
+	
 
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
 
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
 
 	$scope.brands = {
 		model: null,
