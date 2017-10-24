5414d838d397313476d7b1a1f3c200405afc1861
Stuart <stuart.dixon@gmail.com>
2017-10-23 20:30:24 -0700

    variation selector

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,35 +1,19 @@
 webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filter', function($scope, $modal, $http, $filter){
 
 	$scope.optly_api_base = "https://api.optimizely.com/v2";
+	$scope.error=false;
+	$http.defaults.headers.common['Authorization'] = ""
 
 	if (readCookie('optlyCred')) {
 		$http.defaults.headers.common['Authorization'] = readCookie('optlyCred')
 	} else {
 		$http.defaults.headers.common['Authorization'] = prompt("enter API token")
-		if ($http.defaults.headers.common['Authorization'] != null) {
+		if ($http.defaults.headers.common['Authorization'] != null && $http.defaults.headers.common['Authorization'].length != 0) {
 			createCookie ('optlyCred', $http.defaults.headers.common['Authorization'], 2)
+		} else {
+			$scope.error=true;
+			$scope.error_message = "You need to enter a personal optimizely token. Details: https://developers.optimizely.com/x/authentication/personal-token/"
 		}
-	}
-	
-	function createCookie(name,value,days) {
-	    var expires = "";
-	    if (days) {
-	        var date = new Date();
-	        date.setTime(date.getTime() + (days*24*60*60*1000));
-	        expires = "; expires=" + date.toUTCString();
-	    }
-	    document.cookie = name + "=" + value + expires + "; path=/";
-	}
-
-	function readCookie(name) {
-	    var nameEQ = name + "=";
-	    var ca = document.cookie.split(';');
-	    for(var i=0;i < ca.length;i++) {
-	        var c = ca[i];
-	        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-	        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	    }
-	    return null;
 	}
 
 	$scope.variation_ids = [];
@@ -66,9 +50,6 @@ webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filte
 
 	$scope.getPages = function(date) {
 		$scope.data = {}
-		//$scope.campaigns.visibility = false;
-		//$scope.pages.visibility = false;
-		//$scope.audiences.visibility = false;
 
 		$scope.campaigns = {
 			model: null,
@@ -97,12 +78,22 @@ webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filte
 				})
 			}
 			$scope.pages.visibility = true;
-		});	
+		}, function(response){
+			console.log(response)
+				if(response.data.code="invalid_credentials") {
+					$scope.error_message = "Invalid credentials. Refresh the page and enter a better API token. Format is usually \"Bearer XXXXXXXXX\"";
+        	$scope.error=true
+        	eraseCookie("optlyCred")
+				}
+        
+  	});	
 	}
 
  $scope.getExperiments = function() {
  	$scope.campaigns.visibility = true;
  	$scope.campaigns.loading = true;
+ 	$scope.audiences.availableOptions = [];
+
 	$http.get($scope.optly_api_base+'/campaigns?project_id='+$scope.brands.model.project_id)
     .then(function(response) {
     	$scope.data.campaigns = response.data
@@ -144,8 +135,7 @@ webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filte
 				$scope.data.experiments = final_array
 				
 
-				//assoc
-				
+				//associations:
 				for(x=0;x<$scope.data.campaigns.length;x++) { //x = campaigns
 					$scope.data.campaigns[x].experiments = []	
 					for(y=0;y<$scope.data.campaigns[x].experiment_priorities.length;y++){ //y = experiment_priorities
@@ -181,30 +171,14 @@ webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filte
 					$scope.audiences.visibility = true;
 					$scope.campaigns.visibility = true;
 
-/*
-	
-	      for(x=0;x<response.data.length;x++) {
-	      	for(y=0;y<$scope.audiences.model.length;y++) {
-	      		if (typeof response.data[x].audience_conditions != "undefined") {
-	        		var aid = $scope.audiences.model[y];
-	        		var str = response.data[x].audience_conditions;
-	        		if(str.match(aid)) {
-	        			for(z=0;z<response.data[x].variations.length;z++) {
-	        				$scope.variation_ids.push(response.data[x].variations[z].variation_id)
-	        			}
-	        		}
-	        	}	
-	      	}
-	      }
-*/
 					$scope.$watchCollection( 'variation_ids', function(newVal){
         		for( var i = 0; i < newVal.length; ++i ) {
         		console.log(newVal[i]); 
         		}
     			});
+
 		      // create the appropriate URL
-		      $scope.getUrl = function () {
-		      	
+		      $scope.getUrl = function () {	
 			      if($scope.pages.model.edit_url.match(/browse/) && !$scope.pages.model.edit_url.match(/home/)) {
 			      	var str = $scope.pages.model.edit_url;
 			      	$scope.path = str.substring(str.indexOf(".com")+4,str.length)+"&";
@@ -216,10 +190,8 @@ webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filte
 			      }
 
 
-
 			      $scope.brandUrl = "http://www."+$scope.brands.model.wip_subdomain+".wip.gidapps.com"
 			      $scope.targetURL = $scope.brandUrl + $scope.path+"optimizely_token=PUBLIC&optimizely_x_audiences="+$scope.audiences.model.join(',')+"&optimizely_x="+$scope.variation_ids.join(',')
-			      //$scope.targetURL = "TBD"
 			      $scope.final_url = $scope.brandUrl+"/preview?date="+$filter('date')($scope.date.dateDropDownInput, 'MM/dd/yyyy')+"&targetURL="+encodeURIComponent($scope.targetURL)
 					}
 
@@ -228,12 +200,29 @@ webApp.controller('HomepageController', ['$scope', '$uibModal', '$http', '$filte
     });
   }
 
-
-
-	//$scope.pages = JSON.parse(JSON.stringify($scope.projects));
-	//$scope.audiences = JSON.parse(JSON.stringify($scope.projects));
-	//$scope.campaigns = JSON.parse(JSON.stringify($scope.projects));
-    
-	
-
 }]);
+
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
